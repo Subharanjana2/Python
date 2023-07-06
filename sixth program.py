3bf2d56313450ee6b0b375bad5987ef32deb3d96
Subharanjana2 <123657658+Subharanjana2@users.noreply.github.com>
2023-07-06 16:52:29 +0530

    Delete sixth program.py

--- sixth program.py
+++
@@ -1,9 +0,0 @@
-def iterate_dictionary(dictionary):
-    for key in dictionary:
-        value = dictionary[key]
-        print(key, ":", value)
-
-
-# Example usage
-my_dictionary = {"name": "John", "age": 25, "city": "New York"}
-iterate_dictionary(my_dictionary)
